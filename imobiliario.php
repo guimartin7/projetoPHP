b1da537a9045360ab3b68c6773b592c53e3e4d01
Guilherme <guilherme.martin@outlook.com>
2024-01-04 23:00:38 -0300

    Criação das Páginas

---
+++ imobiliario.php
@@ -0,0 +1,15 @@
+<?php
+    require_once 'rota/init.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fornecedores: Imobiliario</title>
+</head>
+<body>
+    
+</body>
+</html>
